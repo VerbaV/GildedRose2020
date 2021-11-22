5167be16e7fc5c1f2bdbf9a5c61d96a3049099ae
VerbaV <veera.portti@gmail.com>
2021-11-22 13:19:36 +0200

    Created tests in file GildedRoseTest.java

--- src/fi/oulu/tol/sqat/tests/GildedRoseTest.java
+++ src/fi/oulu/tol/sqat/tests/GildedRoseTest.java
@@ -1,7 +1,14 @@
 package fi.oulu.tol.sqat.tests;
 
 import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
 import org.junit.Test;
+
+import fi.oulu.tol.sqat.GildedRose;
+import fi.oulu.tol.sqat.Item;
 
 public class GildedRoseTest {
 
@@ -9,4 +16,168 @@ public class GildedRoseTest {
 	public void testTheTruth() {
 		assertTrue(true);
 	}
+	@Test
+	public void exampleTest() {
+		//create an inn, add an item, and simulate one day
+		GildedRose inn = new GildedRose();
+		inn.setItem(new Item("+5 Dexterity Vest", 10, 20));
+		inn.oneDay();
+		
+		//access a list of items, get the quality of the one set
+		List<Item> items = inn.getItems();
+		int quality = items.get(0).getQuality();
+		
+		//assert quality has decreased by one
+		assertEquals(19, quality, "Failed quality for Dexterity Vest");
+	}
+	
+	@Test
+	public void testGildedRose_SellInValueDecrease() {
+		GildedRose inn = new GildedRose();
+		inn.setItem(new Item("+5 Dexterity Vest", 10, 20));
+		inn.oneDay();
+
+		List<Item> items = inn.getItems();
+		int sellIn = items.get(0).getSellIn();
+		
+		assertEquals(9, sellIn, "Failed sellIn value for Dexterity Vest");
+	}
+	
+	@Test
+	public void testGildedRose_mainPrint() {
+		GildedRose inn= new GildedRose();
+		
+	}
+	
+	@Test
+	public void testGildedRose_updateQualityOfAgedBrie() {
+		GildedRose inn = new GildedRose();
+		inn.setItem(new Item("Aged Brie", 2, 0));
+		inn.oneDay();
+		
+		List<Item> items = inn.getItems();
+		int quality = items.get(0).getQuality();
+		
+		assertEquals(1, quality, "Failed quality for Aged Brie");
+	}
+	
+	@Test
+	public void testGildedRose_QualityOfBacstagePassesForSellInValue10() {
+		GildedRose inn = new GildedRose();
+		inn.setItem(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
+		inn.oneDay();
+		inn.oneDay();
+		inn.oneDay();
+		inn.oneDay();
+		inn.oneDay();
+		inn.oneDay();
+		inn.oneDay();
+		inn.oneDay();
+		inn.oneDay();
+		inn.oneDay();
+		
+		List<Item> items = inn.getItems();
+		int quality = items.get(0).getQuality();
+		
+		assertEquals(22, quality, "Failed quality for Backstage passes to a TAFKAL80ETC concert");
+	}
+	
+	@Test
+	public void testGildedRose_QualityOfBacstagePassesForSellInValue5() {
+		GildedRose inn = new GildedRose();
+		inn.setItem(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
+		inn.oneDay();
+		inn.oneDay();
+		inn.oneDay();
+		inn.oneDay();
+		inn.oneDay();
+		inn.oneDay();
+		inn.oneDay();
+		inn.oneDay();
+		inn.oneDay();
+		inn.oneDay();
+		inn.oneDay();
+		inn.oneDay();
+		inn.oneDay();
+		inn.oneDay();
+		inn.oneDay();
+		
+		List<Item> items = inn.getItems();
+		int quality = items.get(0).getQuality();
+		
+		assertEquals(33, quality, "Failed quality for Backstage passes to a TAFKAL80ETC concert");
+	}
+	
+	@Test
+	public void testGildedRose_negativeValuesOfQuality() {
+		GildedRose inn = new GildedRose();
+		inn.setItem(new Item("Conjured Mana Cake", 3, 6));
+		inn.oneDay();
+		inn.oneDay();
+		inn.oneDay();
+		inn.oneDay();
+		
+		List<Item> items = inn.getItems();
+		int quality = items.get(0).getQuality();
+		
+		assertEquals(0, quality, "Failed quality for Conjured Mana Cake");
+	}
+	
+	@Test
+	public void testGildedRose_sulfurasQuality() {
+		GildedRose inn = new GildedRose();
+		inn.setItem(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
+		inn.oneDay();
+		
+		List<Item> items = inn.getItems();
+		int quality = items.get(0).getQuality();
+		
+		assertEquals(80, quality, "Failed quality for Sulfuras, Hand of Ragnaros");
+	}
+	
+	@Test
+	public void testGildedRose_sulfurasSellInValue() {
+		GildedRose inn = new GildedRose();
+		inn.setItem(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
+		inn.oneDay();
+		
+		List<Item> items = inn.getItems();
+		int sellIn = items.get(0).getSellIn();
+		
+		assertEquals(0, sellIn, "Failed sellIn value for Sulfuras, Hand of Ragnaros");
+	}
+	
+	@Test
+	public void testGildedRose_QualityAfterSellInValueGoesUnderZero() {
+		GildedRose inn = new GildedRose();
+		inn.setItem(new Item("Conjured Mana Cake", 3, 6));
+		inn.oneDay();
+		inn.oneDay();
+		inn.oneDay();
+		inn.oneDay();
+		
+		List<Item> items = inn.getItems();
+		int quality = items.get(0).getQuality();
+		
+		assertEquals(1, quality, "Failed quality for Conjured Mana Cake");
+	}
+	
+	@Test
+	public void testGildedRose_QualityUnderZero() {
+		GildedRose inn = new GildedRose();
+		inn.setItem(new Item("Conjured Mana Cake", 3, 6));
+		inn.oneDay();
+		inn.oneDay();
+		inn.oneDay();
+		inn.oneDay();
+		inn.oneDay();
+		inn.oneDay();
+		
+		List<Item> items = inn.getItems();
+		int quality = items.get(0).getQuality();
+		
+		assertEquals(0, quality, "Failed quality for Conjured Mana Cake");
+	}
+	
+	
 }
